a19c378fcfb1e194c33d5f9dd53d428bca93ec67
eldoradodo <rla1eh2dus3@naver.com>
2024-05-20 15:52:40 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 export default function App() {
+
+  const [ count, setCount ] = useState(0);
+
+  const plus = () => setCount(count + number );
+  const minus = () => setCount(count - number ); 
+
+  const onChange = (e) => setCount(e.target.value);
+  
+  const onReset = () => setCount(0);
+  
+  if (!count .trim())
+    return alert ("숫자를 입력해주세요.");
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input type="number" placeholder="숫자" onChange={onChange}/> 만큼을 
+        <button onClick={plus} >더할게요</button> 
+        <button onClick={minus}>뺄게요</button>
+        <button onClick={onReset}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{count}</p>
       </div>
     </div>
   );
